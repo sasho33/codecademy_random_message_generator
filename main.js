9d26381ac413378a17e1fce02496f77f383afba7
sasho33 <sasho3333333@gmail.com>
2023-09-25 12:19:28 +0100

    phrases, authors and themes created

---
+++ main.js
@@ -0,0 +1,37 @@
+const phrases = [
+  'Believe in yourself.',
+  'You are capable of greatness.',
+  'Success comes from hard work.',
+  'The only limit is your imagination.',
+  'Dream big and never give up.',
+  'Every day is a new opportunity.',
+  'Stay positive and keep moving forward.',
+  'Challenges are opportunities in disguise.',
+  'Your time is now.',
+];
+
+const authors = [
+  'Anonymous',
+  'Walt Disney',
+  'Albert Einstein',
+  'Oprah Winfrey',
+  'Nelson Mandela',
+  'Steve Jobs',
+  'Eleanor Roosevelt',
+  'Henry Ford',
+  'Maya Angelou',
+  'Confucius',
+];
+
+const themes = [
+  'Perseverance',
+  'Positivity',
+  'Determination',
+  'Courage',
+  'Ambition',
+  'Optimism',
+  'Resilience',
+  'Leadership',
+  'Inspiration',
+  'Wisdom',
+];
